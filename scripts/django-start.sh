1cda4a695a5b98ded9edda96bf90443f0b9e5aad
Marvin <marvin.tan@wisc.edu>
2021-11-02 17:00:12 -0500

    run unit test when deploy to docker

--- scripts/django-start.sh
+++ scripts/django-start.sh
@@ -7,5 +7,7 @@ while ! pg_isready -h atfoc_db_host ; do
 done
  
 >&2 echo "Postgres is up - executing command"
+# always install the requirements to make sure it is up to date
+python -m pip install -r requirements.txt
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
