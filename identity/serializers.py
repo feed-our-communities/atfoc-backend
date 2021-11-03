10c6cd1195ff01b92ce3781bb2065691e83c8e6f
Michael Messer <16529951+michaelmesser@users.noreply.github.com>
2021-11-02 23:17:11 -0500

    Use email

--- identity/serializers.py
+++ identity/serializers.py
@@ -2,20 +2,21 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from identity.models import UserProfile
 from rest_framework.validators import UniqueValidator
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import authenticate
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
-    username = serializers.CharField( validators=[UniqueValidator(queryset=User.objects.all())], required=True)
+    email = serializers.CharField(validators=[UniqueValidator(queryset=User.objects.all())], required=True)
  
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['id', 'password', 'email']
 
     def create(self, validated_data):
-        username = validated_data['username']
         email = validated_data['email']
         password = validated_data['password']
-        user = User.objects.create_user(username, email, password)
+        user = User.objects.create_user(email, email, password)
         
         profile = UserProfile.objects.create(user=user)
         profile.save()
@@ -24,6 +25,42 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         return_rep = dict()
         return_rep['id'] = instance.id
-        return_rep['username'] = instance.username
         return_rep['email'] = instance.email
         return return_rep
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(
+        label=_("Email"),
+        write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label=_("Token"),
+        read_only=True
+    )
+
+    def validate(self, attrs):
+        username = attrs.get('email')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- identity/views.py
+++ identity/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, CustomAuthTokenSerializer
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
 from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -21,3 +24,17 @@ class UserInfoView(RetrieveAPIView):
       queryset = self.get_queryset()
       obj = get_object_or_404(queryset, username=self.request.user.username)
       return obj
+
+class CustomAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
