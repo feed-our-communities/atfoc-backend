35d97d7aa1e4b99acb25739438549ee1e30701bd
Marvin <marvin.tan@wisc.edu>
2021-11-02 23:13:33 -0500

    remove TTY error

--- scripts/restart_docker.sh
+++ scripts/restart_docker.sh
@@ -2,4 +2,4 @@ docker stop $(docker ps -a | grep atfoc_db | awk '{print $1}')
 docker stop $(docker ps -a | grep atfoc_web | awk '{print $1}')
 docker-compose up -d
 docker ps
-docker exec -it $(docker ps -a | grep atfoc_web | awk '{print $1}') pytest identity
+docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') pytest identity
--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
 docker ps
-docker exec -it $(docker ps -a | grep atfoc_web | awk '{print $1}') pytest identity
+docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') pytest identity
