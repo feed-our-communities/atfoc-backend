3cec1c3601ef04eb043b4a9edb32ced33fec070e
Marvin <marvin.tan@wisc.edu>
2021-11-02 22:54:40 -0500

    add migrations

--- scripts/django-start.sh
+++ scripts/django-start.sh
@@ -9,5 +9,6 @@ done
 >&2 echo "Postgres is up - executing command"
 # always install the requirements to make sure it is up to date
 python -m pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
