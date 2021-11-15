b8857a36c60410a3da1d5d78bb7096bb89c2135a
Marvin <marvin.tan@wisc.edu>
2021-11-15 01:22:18 -0600

    add extra sleep time

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,4 +1,5 @@
 docker-compose up -d
 docker ps
+sleep 5
 docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') pytest identity
 docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') pytest listing
