285cb1d7946566261e55c36fb92430e0d88dd7d2
Marvin <marvin.tan@wisc.edu>
2021-11-29 13:09:24 -0600

    better formatted code coverage report...

--- scripts/coverages.sh
+++ scripts/coverages.sh
@@ -1,4 +1,6 @@
-docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') coverage run --branch -m pytest -vvv identity listing
+docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') coverage run --branch -m pytest -vvv identity listing > /dev/null
+echo '==================branch coverage=================='
 docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') coverage report -m 
-docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') coverage run -m pytest -vvv identity listing
+docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') coverage run -m pytest -vvv identity listing > /dev/null
+echo '==================statement coverage=================='
 docker exec -i $(docker ps -a | grep atfoc_web | awk '{print $1}') coverage report -m 
